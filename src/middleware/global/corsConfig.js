ad6aba10a67a20e3ddd1bc2b69a97be946ec1933
_vishwaraj <vishwarajchundawat008@gmail.com>
2025-08-11 15:30:09 +0530

    minior errors

--- src/middleware/global/corsConfig.js
+++ src/middleware/global/corsConfig.js
@@ -4,8 +4,6 @@ const originsByEnv = {
   development: [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://localhost:5173",
-    "http://ai.nds.studio",
   ],
   production: [""],
 };
